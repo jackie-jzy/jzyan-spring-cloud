0275745f9cf497837e91e1d728229e09e00aa7c1
jackie <jiangzhongyan@xyb100.com>
2020-05-22 16:55:12 +0800

    集成jpa

--- jzyan-oauth2-cloud/oauth2-service/src/main/java/cn/jzyan/oauth2/repository/UserRepository.java
+++ jzyan-oauth2-cloud/oauth2-service/src/main/java/cn/jzyan/oauth2/repository/UserRepository.java
@@ -2,6 +2,7 @@ package cn.jzyan.oauth2.repository;
 
 import cn.jzyan.oauth2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.Optional;
  * @CreateDate : 2020/05/15 10:21
  */
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
     @Override
     List<User> findAll();
